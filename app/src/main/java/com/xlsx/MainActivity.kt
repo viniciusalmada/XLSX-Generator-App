14cfa3583a9ed8ff1ed064604b2f5cb12b718853
Vinicius GHApp <viniciusalmada10@gmail.com>
2020-03-05 07:38:54 -0300

    Now works

--- app/src/main/java/com/xlsx/MainActivity.kt
+++ app/src/main/java/com/xlsx/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.xlsx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import android.os.Environment
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import org.apache.poi.ss.usermodel.IndexedColors
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.File
 import java.io.FileOutputStream
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +20,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        System.setProperty(
+            "org.apache.poi.javax.xml.stream.XMLInputFactory",
+            "com.fasterxml.aalto.stax.InputFactoryImpl"
+        )
+        System.setProperty(
+            "org.apache.poi.javax.xml.stream.XMLOutputFactory",
+            "com.fasterxml.aalto.stax.OutputFactoryImpl"
+        )
+        System.setProperty(
+            "org.apache.poi.javax.xml.stream.XMLEventFactory",
+            "com.fasterxml.aalto.stax.EventFactoryImpl"
+        )
     }
 
     override fun onResume() {
         super.onResume()
 
+        val path = cacheDir
+
+//        System.setProperty(
+//            "org.apache.poi.javax.xml.stream.XMLOutputFactory",
+//            "com.fasterxml.aalto.stax.OutputFactoryImpl"
+//        )
+//        System.setProperty(
+//            "org.apache.poi.javax.xml.stream.XMLEventFactory",
+//            "com.fasterxml.aalto.stax.EventFactoryImpl"
+//        )
+
         val persons = listOf<Person>(
-            Person("Vinicius","Almada", 22),
-            Person("Mohara","Nascimento", 23),
-            Person("Pepita","Nascimento Almada", 1)
+            Person("Vinicius", "Almada", 22),
+            Person("Mohara", "Nascimento", 23),
+            Person("Pepita", "Nascimento Almada", 1)
         )
 
         val workbook = XSSFWorkbook()
@@ -46,7 +74,7 @@ class MainActivity : AppCompatActivity() {
             cell.cellStyle = headerCellStyle
         }
 
-        var rowNum = 1;
+        var rowNum = 1
 
         persons.forEach {
             val r = sheet.createRow(rowNum++)
@@ -54,13 +82,22 @@ class MainActivity : AppCompatActivity() {
             r.createCell(1).setCellValue(it.lastName)
             r.createCell(2).setCellValue(it.age.toDouble())
         }
-
+        
         COLUMNS.indices.forEach {
-            sheet.autoSizeColumn(it)
+            //            sheet.autoSizeColumn(it)
+        }
+        try {
+            val output = FileOutputStream(
+                File(path, "persons.xlxs")
+            )
+            workbook.write(output)
+            output.close()
+
+            Toast.makeText(this,path.absolutePath, Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(this,"error", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
         }
 
-        val output = FileOutputStream("persons.xlxs")
-        workbook.write(output)
-        output.close()
     }
 }
